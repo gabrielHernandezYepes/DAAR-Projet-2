a9fc5e52bceb4c646a9883dbe3df297ab28f400a
gabrielHernandezYepes <gabi92102@gmail.com>
2024-10-12 17:48:33 +0200

    First onchain part- Draft

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -1,12 +1,50 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.0;
 
-contract Collection {
-  string public name;
-  int public cardCount;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-  constructor(string memory _name, int _cardCount) {
-    name = _name;
-    cardCount = _cardCount;
-  }
+contract NFTCollection is ERC721, Ownable {
+    struct CardInfo {
+        uint collectionId;
+        uint cardNumber;
+        string tokenURI;
+    }
+
+    // Mapping de tokenId vers les informations de la carte
+    mapping(uint => CardInfo) public cardInfos;
+
+    // Compteur pour les tokenIds
+    uint private _currentTokenId = 0;
+
+    constructor() ERC721("MyNFTCollection", "MNFT") {}
+
+    // Fonction pour frapper une carte
+    function mintCard(
+        address to,
+        uint collectionId,
+        uint cardNumber,
+        string memory tokenURI
+    ) external onlyOwner returns (uint) {
+        _currentTokenId++;
+        uint newTokenId = _currentTokenId;
+
+        // Frappe le NFT
+        _safeMint(to, newTokenId);
+
+        // Stocke les informations de la carte
+        cardInfos[newTokenId] = CardInfo({
+            collectionId: collectionId,
+            cardNumber: cardNumber,
+            tokenURI: tokenURI
+        });
+
+        return newTokenId;
+    }
+
+    // Surcharge de la fonction tokenURI pour retourner l'URI correcte
+    function tokenURI(uint tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "Token does not exist");
+        return cardInfos[tokenId].tokenURI;
+    }
 }
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -1,17 +1,65 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.0;
 
-import "./Collection.sol";
+import "./NFTCollection.sol";
 
-contract Main {
-  int private count;
-  mapping(int => Collection) private collections;
+contract Main is Ownable {
+    struct CollectionInfo {
+        string name;
+        uint cardCount;
+        uint mintedCount;
+    }
 
-  constructor() {
-    count = 0;
-  }
+    // Mapping de collectionId vers les informations de la collection
+    mapping(uint => CollectionInfo) public collections;
+    uint public collectionCount = 0;
 
-  function createCollection(string calldata name, int cardCount) external {
-    collections[count++] = new Collection(name, cardCount);
-  }
+    NFTCollection public nftContract;
+
+    constructor(address _nftContractAddress) {
+        nftContract = NFTCollection(_nftContractAddress);
+    }
+
+    // Fonction pour créer une nouvelle collection
+    function createCollection(string calldata name, uint cardCount) external onlyOwner {
+        collections[collectionCount] = CollectionInfo({
+            name: name,
+            cardCount: cardCount,
+            mintedCount: 0
+        });
+        collectionCount++;
+    }
+
+    // Fonction pour frapper et assigner des cartes à un utilisateur
+    function mintCardsToUser(
+        uint collectionId,
+        address to,
+        uint[] calldata cardNumbers,
+        string[] calldata tokenURIs
+    ) external onlyOwner {
+        require(collectionId < collectionCount, "Collection does not exist");
+        require(cardNumbers.length == tokenURIs.length, "Mismatched inputs");
+
+        CollectionInfo storage collection = collections[collectionId];
+
+        for (uint i = 0; i < cardNumbers.length; i++) {
+            uint cardNumber = cardNumbers[i];
+
+            require(cardNumber >= 1 && cardNumber <= collection.cardCount, "Invalid card number");
+
+            // Vérifie que la carte n'a pas déjà été frappée
+            // Vous pouvez implémenter un mécanisme pour suivre les cartes déjà frappées
+
+            // Frappe la carte via le contrat NFTCollection
+            nftContract.mintCard(to, collectionId, cardNumber, tokenURIs[i]);
+
+            collection.mintedCount++;
+        }
+    }
+
+    // Fonction pour obtenir les informations d'une collection
+    function getCollection(uint collectionId) external view returns (CollectionInfo memory) {
+        require(collectionId < collectionCount, "Collection does not exist");
+        return collections[collectionId];
+    }
 }
